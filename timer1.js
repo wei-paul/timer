3fcd9104c2a42ef794a33052712e3592b3f65a88
wei-paul <paul.wei607@gmail.com>
2021-09-22 02:19:22 +0000

    fixed timer

--- timer1.js
+++ timer1.js
@@ -1,28 +1,17 @@
-// node timer1.js 10 3 5 15 9 
-
-// This will make it beep at:
-
-// 3 seconds
-// 5 seconds
-// 9 seconds
-// 10 seconds
-// 15 seconds
-
-//convert numbers to seconds
-
 const alarm = function(delay) {
+  let parsed = parseInt(delay);
+   if (isNaN(parsed)) {
+    console.log("Please input a number")
+    return;
+  };
   delay = Number(delay) * 1000
-  if (delay !== "" || delay !== 0 || delay == !isNaN) {
-    
     setTimeout(() => {
       console.log('Beep')
     }, delay)
-  }
-}
+};
 const args = process.argv.slice(2);
   console.log(args);
 
 for (let i = 0; i < args.length; i++) {
   alarm(args[i])
 }
-  
